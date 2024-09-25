9efe2dac7c90b1188218d7df13aa930821371bf6
Sudip Dhakal <dhakalsudip246@gmail.com>
2024-09-25 12:10:12 +0545

    Update binary to string.js
    Comments are added

--- binary to string.js
+++ binary to string.js
@@ -1,12 +1,15 @@
+// it asks user to input binary code 
 const userinp=prompt("Enter any encoded binary code");
 function Result1(resu,userinp){
-    if (userinp===""){
+  // it checks whether user has given data or not
+    if (userinp===""){        
         console.log("No input is given");
     }
     else{
         console.log("The encoded data is:",resu);
     };
-     };
+//      };
+// it converts binary code into ASCII and then into string and finally passes data to Result1
 function main(user){
     let result=user.match(/.{1,8}/g);
     console.log(result);
